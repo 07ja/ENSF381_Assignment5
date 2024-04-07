c50323676715fe19f9a31a5385d8b6eca19a2bf3
Jaimal Sahota <jaimal.sahota@ucalgary.ca>
2024-04-06 23:54:36 -0600

    Updated productlist.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,8 +1,23 @@
 // ProductList.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from'axios';
 import Product from './Product';
 
-const ProductList = ({ products, onAddToCart }) => {
+const ProductList = ({ onAddToCart }) => {
+  const [products, setProducts] = useState ([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('api/products');
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Could not retireve products', error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <div className="product-list">
       {products.map(product => (
