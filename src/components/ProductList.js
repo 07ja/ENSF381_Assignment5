c417da8644fe5052f69e73167dbb563f90e1f91a
XichengW <xicheng.wang@ucalgary.ca>
2024-04-07 12:31:48 -0600

    Roll back version of ProductList, make fetching data from api works

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,23 +1,8 @@
 // ProductList.js
-import React, { useState, useEffect } from 'react';
-import axios from'axios';
+import React from 'react';
 import Product from './Product';
 
-const ProductList = ({ onAddToCart }) => {
-  const [products, setProducts] = useState ([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await axios.get('api/products');
-        setProducts(response.data);
-      } catch (error) {
-        console.error('Could not retireve products', error);
-      }
-    };
-    fetchProducts();
-  }, []);
-
+const ProductList = ({ products, onAddToCart }) => {
   return (
     <div className="product-list">
       {products.map(product => (
